1a9bf1832c9feeb1d54ea8cea1f48e7cb37dd8e9
mohajel <msadegi007@yahoo.com>
2023-03-19 13:11:52 +0330

    backgroung img added

--- main.cpp
+++ main.cpp
@@ -5,27 +5,43 @@ using namespace std;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(500, 500), "SNAKE__GAME");
+    sf::RenderWindow window(sf::VideoMode(540, 540), "SNAKE__GAME");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
     sf::Texture texture;
+    sf::Texture backgroundTexture;
+
+    backgroundTexture.setRepeated(true);
+
+    
+
     if (!texture.loadFromFile("./pictures/SnakeHead.png"))
     {
-        cerr << "can load" << endl;
+        cerr << "cant load" << endl;
+        exit(EXIT_FAILURE);
+    }    backgroundTexture.setSmooth(true);
+    if (!backgroundTexture.loadFromFile("./pictures/Background1.jpg"))
+    {
+        cerr << "cant load" << endl;
         exit(EXIT_FAILURE);
     }
     texture.setSmooth(true);
     sf::Sprite sprite;
+    sf::Sprite backgroundSprite;
+
     sprite.setTexture(texture);
     // sprite.setColor(sf::Color(255, 255, 255, 128)); // half transparent
     sprite.setScale(sf::Vector2f(0.2, 0.2));
+    backgroundSprite.setScale(sf::Vector2f(0.8, 0.8));
 
+    backgroundSprite.setTexture(backgroundTexture);
+
+    backgroundSprite.setTextureRect(sf::IntRect(0 ,0 , 5000, 5000));
     // can use thesee::
     // sprite.setPosition(sf::Vector2f(10.f, 50.f)); // absolute position
     // sprite.move(sf::Vector2f(5.f, 10.f)); // offset relative to the current position
     // sprite.setRotation(90.f); // absolute angle
-
 
     while (window.isOpen())
     {
@@ -81,6 +97,8 @@ int main()
         }
 
         window.clear();
+
+        window.draw(backgroundSprite);
         // window.draw(shape);
         window.draw(sprite);
         window.display();
